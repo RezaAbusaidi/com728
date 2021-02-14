5437a8dff7546427371fac5ebba8d74f27603a0e
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-02-14 21:29:18 +0000

    if elif else added

---
+++ Decisions/if_elif_else.py
@@ -0,0 +1,13 @@
+direction = input('what direction should we move brush?')
+
+if direction == 'up':
+    print('direction is up')
+elif direction == 'down':
+    print('direction is down')
+elif direction == ('left'):
+    print('direction is left')
+elif direction == 'right':
+    print('direction is right')
+else:
+    print('direction is right')
+
