1777f6cd12370cdec1788bbf5bb8d144a6557bba
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-02-20 15:17:31 +0000

    logical or operator

---
+++ Decisions/or_operator.py
@@ -0,0 +1,9 @@
+adventure = input('What type of adventure should I have ')
+if (adventure == 'scary') or (adventure=='short'):
+    print('Entering the dark forrest!')
+
+if (adventure == 'safe') or (adventure == 'long'):
+    print('Taking the safe route!')
+    
+else:
+    print('Not sure which route to take')
