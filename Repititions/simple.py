eea723262390b47cb7ffb71ce4df0ab5de195c79
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-02-20 17:54:04 +0000

    simple while loop with new line after each iteration

---
+++ Repititions/simple.py
@@ -0,0 +1,9 @@
+iteration = 0
+print('how many cables should I remove?')
+cables = input()
+
+
+while iteration < int(cables):
+    iteration = iteration + 1
+
+print(iteration*('\nRemoved cables'))
