f4be6cccb3e9b124d9c588269a5f7d4353e70026
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-02-20 16:18:47 +0000

    logical and operator

---
+++ Decisions/and_operator.py
@@ -0,0 +1,8 @@
+hear = input('what did I hear?')
+
+see = input('what did I see?')
+
+if (hear == 'grr' and (see == 'two red eyes')):
+    print('There is a scary creature. I should get out of here!')
+else:
+    print('I am a little scared but I will continue')
