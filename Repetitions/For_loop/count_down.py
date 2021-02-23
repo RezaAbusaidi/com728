fdcca5bca11148ca6a7474a59e723427b67517e5
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-02-23 20:53:36 +0000

    added comment on using sequence in range

--- Repetitions/For_loop/count_down.py
+++ Repetitions/For_loop/count_down.py
@@ -1,5 +1,6 @@
 steps_left = int(input('how many steps left?'))
 
+#depending which direction you want the loop to count, define start (in our case: user input eg 6). Then we want the count to stop at 0 and count backwards (-1)
 for i in range(steps_left,0,-1):
 
 
