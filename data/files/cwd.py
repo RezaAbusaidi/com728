9447be559665d0d64a0c80b8a7a42eca263150ac
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-03-05 21:31:44 +0000

    current working directory with for loop and os

---
+++ data/files/cwd.py
@@ -0,0 +1,13 @@
+import os
+# path = os.getcwd()
+
+# for file in os.listdir(path):
+#     print(file)
+
+def cwd ():
+    path = os.getcwd() #we define a variable called 'path' and based on that we ask the 'os' to give us the current working directory
+    print(f'The current working directory is {path}')
+    print('The directory contains the following files')
+    for files in os.listdir(path):  # works like the ls command on Linux
+        print(files)
+cwd()
