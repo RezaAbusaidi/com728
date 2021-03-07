db88d320c3f69a22b27605d97c8f93185ebfae1a
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-03-07 15:54:58 +0000

    added strip

--- data/files/txt/read_lines.py
+++ data/files/txt/read_lines.py
@@ -1,4 +1,4 @@
-filename = '/Users/user/Desktop/MSc AI and Data Science /DAT256x/Prepvirtualenvironment3.9.1/bin/python" "/Users/user/Desktop/MSc AI and Data Science /DAT256x/Prepvirtualenvironment3.9.1/com728/data/files/txt/library.txt'
+# filename = '/Users/user/Desktop/MSc AI and Data Science /DAT256x/Prepvirtualenvironment3.9.1/bin/python" "/Users/user/Desktop/MSc AI and Data Science /DAT256x/Prepvirtualenvironment3.9.1/com728/data/files/txt/library.txt'
 
 
 def search(filename):
