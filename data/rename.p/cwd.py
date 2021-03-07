252f62bf2d9771c5cba09792ea05d4b42bfc4ffa
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-03-07 16:02:14 +0000

    no change

--- data/rename.p/cwd.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def cwd ():
-    path = os.getcwd() #we define a variable called 'path' and based on that we ask the 'os' to give us the current working directory
-    print(f'The current working directory is {path}')
-    print('The directory contains the following files')
-    for files in os.listdir(path):  # works like the ls command on Linux
-        print(files)
-cwd()
