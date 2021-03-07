db6f42e59207d0b57122b9d90eb4d00ae6698809
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-03-07 16:00:01 +0000

    no change

---
+++ data/rename.p/txt/read_lines.py
@@ -0,0 +1,18 @@
+# filename = '/Users/user/Desktop/MSc AI and Data Science /DAT256x/Prepvirtualenvironment3.9.1/bin/python" "/Users/user/Desktop/MSc AI and Data Science /DAT256x/Prepvirtualenvironment3.9.1/com728/data/files/txt/library.txt'
+
+
+def search(filename):
+    print('searching ...')
+    with open(filename) as file:
+        for line in file:
+
+            print(f'Looked in The {line.strip()}.')
+
+
+def run():
+    search('library.txt')
+    print('..done')
+
+
+if __name__ == '__main__':
+    run()
