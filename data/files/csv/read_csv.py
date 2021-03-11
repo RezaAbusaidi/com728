cfb1bb993e1037b51d96b880de80e13660fe1779
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-03-11 21:15:30 +0000

    just cleaned up comments

--- data/files/csv/read_csv.py
+++ data/files/csv/read_csv.py
@@ -1,12 +1,6 @@
 import csv
 from typing import ValuesView
 
-# with open('bots.csv') as file:
-#     csv_reader = csv.reader(file)
-#     headings = next(csv_reader)
-#     for values in (file):
-#         print(values)
-
 
 def read(filepath):
     with open('bots.csv') as file:
