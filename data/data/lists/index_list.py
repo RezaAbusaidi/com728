c13c1838c9596c92d123a23edf66ffa265751b69
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-03-13 21:14:40 +0000

    list access in different function returned call

---
+++ data/data/lists/index_list.py
@@ -0,0 +1,13 @@
+def movements():
+    path = ["Move Forward", 10, "Move Backward", 5, "Move Left", 3, "Move Right", 1 ]
+    return path
+def run():
+    print ("Moving...")
+    pathology = movements()
+    print(f"{pathology[0]} for {pathology[1]} steps")
+    print(f"{pathology[2]} for {pathology[3]} steps")
+    print(f"{pathology[4]} for {pathology[5]} steps")
+    print(f"{pathology[6]} for {pathology[7]} steps")
+
+if __name__ == '__main__':
+    run()
