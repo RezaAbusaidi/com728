1b77bc2a2db2cfa577f93968f175d3785f004b05
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-03-13 20:47:52 +0000

    lists and printing returned call of a list from a different function

---
+++ data/data/lists/simple_list.py
@@ -0,0 +1,9 @@
+def directions():
+    direction = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return direction
+
+def run():
+    print(directions())
+    
+if __name__ == "__main__":
+    run()
