531851077cd884a694da22f68d846436184381d6
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-03-14 10:50:10 +0000

    for loop, user input as index , matched with already set out index.

---
+++ data/data/lists/populate_list.py
@@ -0,0 +1,28 @@
+def directions():
+    directions = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return directions
+
+
+def menu():
+    print("Please select a direction")
+    # We call the first function and store the returned list in a local variable
+    available_directions = directions()
+    for i in range(len(available_directions)):
+        print(f"{i} : {available_directions[i]}")
+    # thats the input you get from the user, which is the integer set earlier.
+    userdirection = int(input())
+    # This links them together. Using user-input as an index.
+    return available_directions[userdirection]
+
+
+def run():
+    route = []
+    print("Working out escape route...")
+    for count in range(5):  # remember when returning an object, call the relevant function, the desired object will be called automatically
+        # menu fucntion will retun the user response and append to the mepty list "route". We do route.append(call the function that has the return)
+        route.append(menu())
+        print(f"Escape route: {route}")
+
+
+if __name__ == '__main__':
+    run()
