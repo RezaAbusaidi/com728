a07f289e9e2c83c44eeb8ad44943e5f88d8b344e
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-03-26 19:24:49 +0000

    fetch name and description INNER JOIN

--- persistence/joins/database.py
+++ persistence/joins/database.py
@@ -5,7 +5,7 @@ import sqlite3
 def display_products_with_stock_levels():
     db = sqlite3.connect("catalogue.db")
     cursor = db.cursor()
-    sql = (" SELECT name, description, quantity FROM product NATURAL JOIN stock ")
+    sql = (" SELECT name, description FROM product NATURAL JOIN stock ")
     cursor.execute(sql)
     records = cursor.fetchall()
     print(f'There are {len(records)} product records')
