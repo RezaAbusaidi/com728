64f8105a78e6766bd598e9e2e658173ee01e036a
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-03-26 19:16:35 +0000

    SQL JOIN

---
+++ persistence/joins/database.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+
+
+def display_products_with_stock_levels():
+    db = sqlite3.connect("catalogue.db")
+    cursor = db.cursor()
+    sql = (" SELECT name, description, quantity"\
+        " FROM product NATURAL JOIN stock ")
+    cursor.execute(sql)
+    records = cursor.fetchall()
+    print(f'There are {len(records)} product records')
+
+    for row in records:
+        print(f"Product{records[0]}")
+        print(f"Product{records[1]}")
+        print(f"Product{records[2]}")
+        print("")
+    db.close()
+
+display_products_with_stock_levels()
