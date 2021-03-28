46351e2a2cead7bd0fd63ce17f9e71b9ca8df215
Reza Abusaidi <5abusr89@solent.ac.uk>
2021-03-28 14:40:33 +0100

    simple dictionary

---
+++ data/data/dicts/simple_dict.py
@@ -0,0 +1,10 @@
+def pattern():
+    sequences = {}
+    sequences = {"Short sequence":3, "Medium sequence":2, "Long sequence":1}
+    return sequences
+
+def run():
+    print(pattern())
+
+if __name__ == '__main__':
+    run()
